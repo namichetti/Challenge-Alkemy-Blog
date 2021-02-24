5ca2fe3d0fb819a8af9c08eeac1d6326e36c0b77
Nestor <nestoramichetti@neuronic.com.ar>
2021-02-23 23:22:57 -0300

    Add Post class

---
+++ src/main/java/com/challenge/alkemy/blog/entity/Post.java
@@ -0,0 +1,83 @@
+package com.challenge.alkemy.blog.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+enum Categoria{
+	MÚSICA,
+	POESÍA,
+	TEATRO,
+	FILOSOFÍA,
+	NOVELA
+}
+
+@Entity
+@Table(name="posts")
+public class Post implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String titulo; 
+	private String contenido; 
+	private String imagen;
+	private Categoria categoria;	
+	@CreationTimestamp
+	@Column(name="fecha_creacion")
+	private LocalDateTime fechaCreacion;
+	
+	public Post() {	
+	}
+
+	
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getContenido() {
+		return contenido;
+	}
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+
+}
